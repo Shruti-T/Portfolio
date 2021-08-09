4be0245c661b9092f76b7fc00de934c5fb95b880
Shruti Tiwari <sh03shruti@gmail.com>
2021-08-09 22:02:54 +0530

    working on animation

--- main.js
+++ main.js
@@ -17,3 +17,18 @@ for (let i = 1; i < 7; i++) {
     classToggle();
   });
 }
+
+// const observer = new IntersectionObserver((entries) => {
+//   // Loop over the entries
+//   entries.forEach((entry) => {
+//     // If the element is visible
+//     // console.log(entry.isIntersecting);
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add("animate__slideInLeft");
+//     }
+//   });
+// });
+
+// observer.observe(document.querySelector(".animate-left"));
+// observer.observe(document.querySelector(".animate-right"));
